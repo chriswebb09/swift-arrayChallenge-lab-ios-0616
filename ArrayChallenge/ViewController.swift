e4c946df12aa392bc6e63313597ca71210d548d7
chriswebb09 <chris.webb5249@gmail.com>
2016-07-08 14:51:03 -0400

    Done

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -16,10 +16,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     
+        let numberHalf: [Int] = [6,4,12,4]
+        let itemHalf: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
         
-        
+        makeShoppingList(itemHalf, quantityOfItems: numberHalf)
     }
     
     
-    
+    func makeShoppingList(items:[String], quantityOfItems:[Int]) ->[String] {
+        var returnArray: [String] = []
+        for (index, item) in items.enumerate() {
+            let count: Int = index + 1
+            let numberOfItems: Int = quantityOfItems[index]
+            let newString:String = "\(count). \(numberOfItems) \(items[index])"
+            returnArray.append(newString)
+        }
+        return returnArray
+
+    }
 }
